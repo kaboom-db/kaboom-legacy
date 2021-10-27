7ecb725a1cada4853024a763fe567190012aef9c
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-10-27 21:02:35 +0100

    few db changes

--- comics/admin.py
+++ comics/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Series, Issue
+from .models import Series, Issue, Publisher, Character, Staff
 
 # Register your models here.
 admin.site.register(Series)
 admin.site.register(Issue)
+admin.site.register(Publisher)
+admin.site.register(Character)
+admin.site.register(Staff)
--- comics/models.py
+++ comics/models.py
@@ -1,9 +1,38 @@
 from django.db import models
 from django.db.models.base import Model
 
+position_choices = (
+    ("WRITER", "Writer"),
+    ("PENCILLER", "Penciller"),
+    ("COVER ARTIST", "Cover Artist"),
+    ("INKER", "Inker"),
+    ("VARIANT COVER ARTIST", "Variant Cover Artist"),
+    ("COLORIST", "Colorist"),
+    ("LETTERER", "Letterer"),
+    ("DESIGNER", "Designer"),
+    ("EDITOR", "Editor"),
+    ("EXECUTIVE EDITOR", "Executive Editor"),
+    ("EDITOR-IN-CHIEF", "Editor-in-Chief")
+)
+
 # Create your models here.
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+    logo = models.URLField()
+
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+    alias = models.CharField(max_length=200)
+    description = models.CharField(max_length=1000)
+    image = models.URLField()
+
 class Series(models.Model):
     series_name = models.CharField(max_length=200)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    characters = models.ManyToManyField(Character)
+    image_small = models.URLField()
+    image_medium = models.URLField()
+    image_large = models.URLField()
 
     def __str__(self) -> str:
         return self.series_name
@@ -11,6 +40,15 @@ class Series(models.Model):
 class Issue(models.Model):
     issue_number = models.IntegerField(default=1)
     series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    description = models.CharField(max_length=1000)
+    image_small = models.URLField()
+    image_medium = models.URLField()
+    image_large = models.URLField()
 
     def __str__(self) -> str:
         return str(self.series) + ' #' + str(self.issue_number)
+
+class Staff(models.Model):
+    name = models.CharField(max_length=200)
+    position = models.CharField(max_length=100, choices=position_choices)
+    image = models.URLField()
