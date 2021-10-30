b9bbaf8765a3b3f045c3ccbfe2fa7dbc10409402
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-10-30 10:59:51 +0100

    Create '/comics/year/[year]'

--- src/comics/urls.py
+++ src/comics/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('issues/<int:issue_id>/', views.issue_detail, name='issue_detail'),
     path('publishers/<int:publisher_id>/', views.publisher_detail, name='publisher_detail'),
     path('staff/<int:staff_id>/', views.staff_detail, name='staff_detail'),
-    path('characters/<int:character_id>/', views.character_detail, name='character_detail')
+    path('characters/<int:character_id>/', views.character_detail, name='character_detail'),
+    path('year/<int:year>/', views.series_year, name='series_year')
 ]
--- src/comics/views.py
+++ src/comics/views.py
@@ -27,3 +27,7 @@ def staff_detail(request, staff_id):
 def character_detail(request, character_id):
     character = get_object_or_404(Character, pk=character_id)
     return render(request, 'comics/character_detail.html', {'character': character})
+
+def series_year(request, year):
+    series = Series.objects.filter(year_started=year)
+    return render(request, 'comics/year.html', {'year': year, 'series': series})
