7a6983f8ee9cc599c01ad0b40aefcdad2432f5b7
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-10-31 09:00:25 +0000

    added index

--- src/kaboom/views.py
+++ src/kaboom/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 def index(request):
-    return HttpResponse("<h1>Welcome to KABOOM</h1><p><a href=\"{% url 'comics:index' %}\">Comics</a></p>")
+    return render(request, 'kaboom/index.html')
