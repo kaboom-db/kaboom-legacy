c99d33426d03f44f0d2f5b8481a3f4a62baf1c50
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-10-31 13:34:32 +0000

    add reference to django rest framwork

--- src/kaboom/urls.py
+++ src/kaboom/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('comics/', include('comics.urls'))
+    path('comics/', include('comics.urls')),
+    path('auth/', include('rest_framework.urls'))
 ]
