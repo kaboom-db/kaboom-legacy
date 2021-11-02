76af870deaafef72e132b060d299423f299ac001
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-11-02 20:09:51 +0000

    clean up

--- src/cartoons/cartoons_filters.py
+++ src/cartoons/cartoons_filters.py
@@ -9,20 +9,3 @@ class CartoonFilter(filters.FilterSet):
     class Meta:
         model = Cartoon
         fields = ['name', 'genres', 'network']
-
-# class StaffFilter(filters.FilterSet):
-#     query = filters.filters.CharFilter(field_name='name', lookup_expr='contains')
-#     position = filters.filters.CharFilter(field_name='position', lookup_expr='contains')
-# 
-#     class Meta:
-#         model = Staff
-#         fields = ['name', 'position']
-# 
-# class SeriesFilter(filters.FilterSet):
-#     query = filters.filters.CharFilter(field_name='series_name', lookup_expr='contains')
-#     status = filters.filters.CharFilter(field_name='status', lookup_expr='contains')
-#     year = filters.filters.NumberFilter(field_name='year_started')
-# 
-#     class Meta:
-#         model = Series
-#         fields = ['series_name', 'year_started', 'status']
