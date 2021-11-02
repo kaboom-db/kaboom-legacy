dcbb3bb7993e4d911128e827f2a89fff337459db
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-11-02 19:21:01 +0000

    created cartoon models

---
+++ src/cartoons/models.py
@@ -0,0 +1,78 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+genres = (
+    ("ACTION", "Action"),
+    ("ADVENTURE", "Adventure"),
+    ("BIOGRAPHY", "Biography"),
+    ("COMEDY", "Comedy"),
+    ("CRIME", "Crime"),
+    ("DOCUMENTARY", "Documentary"),
+    ("DRAMA", "Drama"),
+    ("EPIC", "Epic"),
+    ("FAMILY", "Family"),
+    ("FANTASY", "Fantasy"),
+    ("GAME-SHOW", "Game-Show"),
+    ("HISTORY", "History"),
+    ("HORROR", "Horror"),
+    ("MUSICAL", "Musical"),
+    ("MYSTERY", "Mystery"),
+    ("SCI-FI", "Sci-Fi"),
+    ("THRILLER", "Thriller"),
+)
+
+class VoiceActor(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Character(models.Model):
+    name = models.CharField(max_length=200)
+    voice_actor = models.ForeignKey(VoiceActor, on_delete=models.CASCADE)
+    image = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Network(models.Model):
+    name = models.CharField(max_length=200)
+    website = models.URLField(blank=True)
+    logo = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Genre(models.Model):
+    genre = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.genre
+
+class Cartoon(models.Model):
+    name = models.CharField(max_length=200)
+    network = models.ForeignKey(Network, on_delete=models.CASCADE)
+    genres = models.ManyToManyField(Genre, blank=True)
+    summary = models.CharField(max_length=10000)
+    season_count = models.IntegerField(default=1)
+    image_small = models.URLField(blank=True)
+    image_medium = models.URLField(blank=True)
+    image_large = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Episode(models.Model):
+    episode_number = models.IntegerField()
+    absolute_episode_number = models.IntegerField()
+    season_number = models.IntegerField(default=1)
+    cartoon = models.ForeignKey(Cartoon, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    summary = models.CharField(max_length=100)
+    release_date = models.DateTimeField()
+    rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    screenshot = models.URLField(blank=True)
+
+    def __str__(self) -> str:
+        return str(self.cartoon) + " S" + str(self.season_number) + "E" + str(self.episode_number) + ": " + self.name
--- src/comics/models.py
+++ src/comics/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db.models.base import Model
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 position_choices = (
