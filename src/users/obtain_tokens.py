44a4c433eaa75caeb1ea1ab46e2514137a7fd8ad
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-11-04 21:00:00 +0000

    added /login endpoint

---
+++ src/users/obtain_tokens.py
@@ -0,0 +1,23 @@
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from libgravatar import Gravatar
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        g = Gravatar(user.email)
+        image = g.get_image()
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email,
+            'image': image
+        })
--- src/users/urls.py
+++ src/users/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 from rest_framework import routers
+from rest_framework.authtoken import views as rf_views
+from .obtain_tokens import CustomAuthToken
 
 app_name = 'users'
 urlpatterns = [
-    path('signup/', views.CreateUser.as_view(), name='index')
+    path('signup/', views.CreateUser.as_view(), name='index'),
+    path('login/', CustomAuthToken.as_view(), name='token')
 ]
