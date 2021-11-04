3eb87ed713e5075dc793b61354a16c05b0ad489d
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-11-04 20:35:11 +0000

    completed signup not as clean as i wanted it to be

--- src/users/views.py
+++ src/users/views.py
@@ -1,18 +1,21 @@
 from rest_framework.views import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import ParseError
 
 class CreateUser(APIView):
     def post(self, request):
         post_data = request.data
-        print(post_data)
         serializer = UserSerializer(data=post_data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response({
-                'user_id': serializer.data['id'],
-                'username': serializer.data['username']
-            })
+        try:
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({
+                    'user_id': serializer.data['id'],
+                    'username': serializer.data['username']
+                })
+        except KeyError:
+            raise ParseError(detail='You are either missing an email, password or username.')
         return Response({
             serializer.errors
         })
