6c5d2341331327df12bc48b29af9e4c7385b28da
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-11-04 20:46:18 +0000

    saving a user now adds a token

--- src/users/models.py
+++ src/users/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-# def create_user(sender, instance=None, created=False, **kwargs):
-#     if created:
-#         # Create a new bio
-#         
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        # Create a new token
+        Token.objects.create(user=instance)
