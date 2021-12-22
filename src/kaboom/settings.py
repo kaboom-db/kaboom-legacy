c3edf2fc4cd44dc3741578c8fb66374bcc222e4e
crxssed <tanveernajib@gmail.com>
2021-12-22 13:15:03 +0000

    migrated db to postgresql

--- src/kaboom/settings.py
+++ src/kaboom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from . import db_secrets
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'kaboom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': db_secrets.DB_NAME,
+        'USER': db_secrets.DB_USER,
+        'PASSWORD': db_secrets.DB_PASS,
+        'HOST': db_secrets.DB_HOST,
+        'PORT': db_secrets.DB_PORT
     }
 }
 
