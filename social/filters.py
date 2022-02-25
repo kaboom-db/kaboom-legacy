c513d594b6497358289499f804cd9ec443be8427
Tanveer Najib <tanveernajib@gmail.com>
2022-02-25 11:09:31 +0000

    Add TODO(#4)

--- social/filters.py
+++ social/filters.py
@@ -1,6 +1,9 @@
 from django_filters import rest_framework as filters
 from .models import Thought, Comment
 
+# // TODO(#4): Test the date filter
+# //    Date filters in Django a weird. Check if they work.
+
 class ThoughtFilter(filters.FilterSet):
     user = filters.filters.CharFilter(field_name='user', lookup_expr='iexact')
     thought_type = filters.filters.CharFilter(field_name='thought_type', lookup_expr='iexact')
