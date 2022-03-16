c8a0b4ac0bb7932e457891f28fc53b208ac27128
Tanveer Najib <tanveernajib@gmail.com>
2022-03-16 14:06:52 +0000

    fixed thought admin bug

--- social/admin.py
+++ social/admin.py
@@ -7,7 +7,8 @@ class ThoughtAdmin(admin.ModelAdmin):
     list_display = ("title", "user", "date_created")
 
     def get_readonly_fields(self, request, obj=None):
-        return self.readonly_fields + ('date_created')
+        print(self.readonly_fields)
+        return self.readonly_fields + ('date_created',)
 
 @admin.register(models.Comment)
 class CommentAdmin(admin.ModelAdmin):
